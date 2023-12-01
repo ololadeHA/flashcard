171e1ba9250d4ba72a92eaf39545252b7eb11282
Arowolo Haleem <arowolohaleem@gmail.com>
2023-12-01 13:34:37 +0100

    edit js and css file

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,20 @@ $(document).ready(function() {
       $("#js-hidden").toggle();
     });
 
-    $(".clickable-o").click(function() {
-      $("#Operators-showing").show().toggle();
-      $("#Operators-hidden").toggle();
-    });
+    // $(".clickable-o").click(function() {
+    //   $("#Operators-showing").show().toggle();
+    //   $("#Operators-hidden").toggle();
+    // });
 
-    $(".clickable-v").click(function() {
-      $("#Variables-showing").toggle();
-      $("#Variables-hidden").toggle();
-    });
+    // $(".clickable-v").click(function() {
+    //   $("#Variables-showing").toggle();
+    //   $("#Variables-hidden").toggle();
+    // });
 
-    $(".clickable-v-n-c").click(function() {
-      $("#v-n-c-showing").toggle();
-      $("#v-n-c-hidden").toggle();
-    });
+    // $(".clickable-v-n-c").click(function() {
+    //   $("#v-n-c-showing").toggle();
+    //   $("#v-n-c-hidden").toggle();
+    // });
 
   
 });
