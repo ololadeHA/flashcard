a1271969039b18b85774d10c80862bf168c69c3f
Arowolo Haleem <arowolohaleem@gmail.com>
2023-12-02 23:16:47 +0100

    edit js file

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,113 @@ $(document).ready(function() {
       $(".js-hidden").toggle();
     });
 
-    // $(".clickable-o").click(function() {
-    //   $("#Operators-showing").show().toggle();
-    //   $("#Operators-hidden").toggle();
-    // });
+    $(".clickable-o").click(function() {
+      $(".Operators-showing").toggle();
+      $(".Operators-hidden").toggle();
+    });
 
-    // $(".clickable-v").click(function() {
-    //   $("#Variables-showing").toggle();
-    //   $("#Variables-hidden").toggle();
-    // });
+    $(".clickable-v").click(function() {
+      $(".Variables-showing").toggle();
+      $(".Variables-hidden").toggle();
+    });
 
-    // $(".clickable-v-n-c").click(function() {
-    //   $("#v-n-c-showing").toggle();
-    //   $("#v-n-c-hidden").toggle();
-    // });
+    $(".clickable-v-n-c").click(function() {
+      $(".v-n-c-showing").toggle();
+      $(".v-n-c-hidden").toggle();
+    });
+
+    $(".clickable-f").click(function() {
+      $(".Functions-showing").toggle();
+      $(".Functions-hidden").toggle();
+    });
+
+    $(".clickable-m").click(function() {
+      $(".Methods-showing").toggle();
+      $(".Methods-hidden").toggle();
+    });
+
+    $(".clickable-arg").click(function() {
+      $(".Arguments-showing").toggle();
+      $(".Arguments-hidden").toggle();
+    });
+
+    $(".clickable-pmt").click(function() {
+      $(".Parameters-showing").toggle();
+      $(".Parameters-hidden").toggle();
+    });
+
+    $(".clickable-rtn").click(function() {
+      $(".Return-showing").toggle();
+      $(".Return-hidden").toggle();
+    });
+
+    $(".clickable-c").click(function() {
+      $(".chain-showing").toggle();
+      $(".chain-hidden").toggle();
+    });
+
+
+    $(".clickable-str").click(function() {
+      $(".strings-showing").toggle();
+      $(".string-hidden").toggle();
+    });
+
+
+    $(".clickable-boo").click(function() {
+      $(".boolean-showing").toggle();
+      $(".boolean-hidden").toggle();
+    });
+
+
+    $(".clickable-v-n-c").click(function() {
+      $("#v-n-c-showing").toggle();
+      $("#v-n-c-hidden").toggle();
+    });
+
+
+    $(".clickable-v-n-c").click(function() {
+      $("#v-n-c-showing").toggle();
+      $("#v-n-c-hidden").toggle();
+    });
+
+
+    $(".clickable-v-n-c").click(function() {
+      $("#v-n-c-showing").toggle();
+      $("#v-n-c-hidden").toggle();
+    });
+
+
+    $(".clickable-v-n-c").click(function() {
+      $("#v-n-c-showing").toggle();
+      $("#v-n-c-hidden").toggle();
+    });
+
+
+    $(".clickable-v-n-c").click(function() {
+      $("#v-n-c-showing").toggle();
+      $("#v-n-c-hidden").toggle();
+    });
+
+
+    $(".clickable-v-n-c").click(function() {
+      $("#v-n-c-showing").toggle();
+      $("#v-n-c-hidden").toggle();
+    });
+
+
+    $(".clickable-v-n-c").click(function() {
+      $("#v-n-c-showing").toggle();
+      $("#v-n-c-hidden").toggle();
+    });
+
+
+    $(".clickable-v-n-c").click(function() {
+      $("#v-n-c-showing").toggle();
+      $("#v-n-c-hidden").toggle();
+    });
+
+
+    
 
   
 });
